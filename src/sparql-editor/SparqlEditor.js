6ded54faeb0549118a6de0c22e6a86a17a40189b
Thomas Minier <tminier01@gmail.com>
2018-08-04 13:08:50 +0200

    urls and queries menus are now build auto. using void descriptors

--- src/sparql-editor/SparqlEditor.js
+++ src/sparql-editor/SparqlEditor.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import QueryExecutor from '../query-executor/QueryExecutor.js'
 import YASQE from 'yasgui-yasqe'
+import { HYDRA, SAGE, SD, RDF, RDFS } from '../rdf.js'
 import 'yasgui-yasqe/dist/yasqe.min.css'
 import './SparqlEditor.css'
 
@@ -13,13 +14,22 @@ import './SparqlEditor.css'
 class SparqlEditor extends Component {
   constructor (props) {
     super(props)
-    this.state = {
-      query: 'select * where { ?s ?p ?o }',
-      url: 'http://sage.univ-nantes.fr/sparql/dbpedia-2016-04'
+    this.serverUrl = 'http://localhost:8000'
+    if (this.serverUrl.endsWith('/')) {
+      this.serverUrl = this.serverUrl.substring(0, this.serverUrl.length - 1)
     }
-
+    this.state = {
+      query: '',
+      queryName: '',
+      url: '',
+      urls: [],
+      queries: []
+    }
     this.updateUrl = this.updateUrl.bind(this)
+    this.setUrl = this.setUrl.bind(this)
+    this.setPresetQuery = this.setPresetQuery.bind(this)
   }
+
   render () {
     return (
       <div className='SparqlEditor'>
@@ -33,13 +43,32 @@ class SparqlEditor extends Component {
                     Select server
                   </button>
                   <div className='dropdown-menu' aria-labelledby='dropdownMenuButton'>
-                    <a className='dropdown-item' href='#'>Action</a>
-                    <a className='dropdown-item' href='#'>Another action</a>
-                    <a className='dropdown-item' href='#'>Something else here</a>
+                    {this.state.urls.map(url => (
+                      <a className='dropdown-item' key={url} href={url} onClick={this.setUrl}>{url}</a>
+                    ))}
                   </div>
                 </div>
               </div>
               <input className='form-control' id='serverInput' type='text' value={this.state.url} onChange={this.updateUrl} />
+            </div>
+          </div>
+
+          <div className='form-group'>
+            <label for='queryName'><strong>Preset query:</strong></label>
+            <div className='input-group'>
+              <div className='input-group-prepend'>
+                <div className='dropdown'>
+                  <button className='btn btn-outline-secondary dropdown-toggle' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                    Preset queries
+                  </button>
+                  <div className='dropdown-menu' aria-labelledby='dropdownMenuButton'>
+                    {this.state.queries.map(q => (
+                      <a className='dropdown-item' key={q.name} qValue={q.value} href={q.url} onClick={this.setPresetQuery}>{q.name}</a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <input className='form-control' id='queryName' type='text' value={this.state.queryName} disabled />
             </div>
           </div>
         </form>
@@ -50,8 +79,12 @@ class SparqlEditor extends Component {
   }
 
   componentDidMount () {
+    // download VoID descriptors to build menus
+    this._getVoidDescriptor(this.serverUrl)
+      .then(data => this._buildMenus(data))
+    // setup YASQE text editor
     this.yasqe = YASQE.fromTextArea(document.getElementById('yasqe-editor'))
-    this.yasqe.setValue(this.props.query || this.state.query)
+    this.yasqe.setValue(this.state.query)
     // keep SPARQL query updated in component state
     this.yasqe.on('change', args => {
       this.setState({
@@ -64,9 +97,76 @@ class SparqlEditor extends Component {
     this.yasqe.refresh()
   }
 
+  _getVoidDescriptor (baseUrl) {
+    const headers = new Headers()
+    headers.append('Accept', 'application/json')
+    const request = new Request(`${baseUrl}/void`, {
+      headers
+    })
+    return fetch(request)
+      .then(res => res.json())
+      .then(body => {
+        const map = new Map()
+        body.forEach(entity => {
+          map.set(entity['@id'], entity)
+        })
+        return map
+      })
+  }
+
+  _buildMenus (descriptor) {
+    const root = descriptor.get(this.serverUrl)
+    const graphCollec = root[SD('availableGraphs')][0]
+    // get all graphs provided by the Sage endpoint
+    const graphs = descriptor
+      .get(graphCollec['@id'])[SD('namedGraph')]
+      .map(g => {
+        const id = descriptor.get(g['@id'])[SD('graph')][0]['@id']
+        return descriptor.get(id)
+      })
+    const urls = graphs.map(g => g[HYDRA('entrypoint')][0]['@id']).sort()
+    const queries = []
+    graphs.forEach(g => {
+      if (SAGE('hasExampleQuery') in g) {
+        g[SAGE('hasExampleQuery')]
+          .map(x => descriptor.get(x['@id']))
+          .sort()
+          .forEach(q => {
+            queries.push({
+              url: g[HYDRA('entrypoint')][0]['@id'],
+              name: q[RDFS('label')][0]['@value'],
+              value: q[RDF('value')][0]['@value']
+            })
+          })
+      }
+    })
+    this.setState({
+      url: urls[0],
+      urls,
+      queries
+    })
+  }
+
   updateUrl (event) {
     this.setState({
       url: event.target.value
+    })
+  }
+
+  setUrl (event) {
+    event.preventDefault()
+    this.setState({
+      url: event.target.href
+    })
+  }
+
+  setPresetQuery (event) {
+    event.preventDefault()
+    this.yasqe.setValue(event.target.attributes.getNamedItem('qvalue').value)
+    this.setState({
+      url: event.target.href,
+      queryName: event.target.innerText,
+      query: event.target.attributes.getNamedItem('qvalue').value
     })
   }
 }
