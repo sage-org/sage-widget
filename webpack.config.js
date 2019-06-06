f5f92da53e852fbab0b0a87ec520d60ef58e85c0
Thomas Minier <tminier01@gmail.com>
2019-06-06 14:47:05 +0200

    switch webpack to production mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
